951f7a847a2d437ea70f06d9282da7b8d19654c0
VIPUL SINGH <120162007+VIPULSINGH18@users.noreply.github.com>
2024-06-15 17:48:27 +0530

    Add files via upload

---
+++ Lec10-CipherSchools.cpp
@@ -0,0 +1,28 @@
+/*
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j,k;
+	i=0;
+	j=0;
+	k=0;
+	cout<<i<<" "<<j<<" "<<k<<" "<<endl;
+	j= i++;
+	cout<<i<<" "<<j<<" "<<k<<" "<<endl;
+	k= ++i;
+	cout<<i<<" "<<j<<" "<<k<<" "<<endl;
+	return 0;
+}
+*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i;
+	for(i=0;i<=100;i++){
+		cout<<i<<" ";
+	}
+	return 0;
+}
